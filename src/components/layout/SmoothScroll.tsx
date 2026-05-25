d81ee3214b2ec6cb55e30661f1f571f042f91d2a
thiago baltrons <thiagobalrami17@gmail.com>
2026-05-25 16:42:59 -0300

    fix: delte lf

--- src/components/layout/SmoothScroll.tsx
+++ src/components/layout/SmoothScroll.tsx
@@ -8,8 +8,6 @@ export function SmoothScroll({ children }: { children: React.ReactNode }) {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // expo.out
-      direction: "vertical",
-      gestureDirection: "vertical",
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 2,
